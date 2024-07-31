25e4359102674bc91bd6c30525416ce3b0e3cac3
Mihaela <mihaelabataklieva2006@gmail.com>
2024-07-31 15:10:58 +0300

    Adding encoding for path

--- src/main/java/com/example/trackit/service/SavingService.java
+++ src/main/java/com/example/trackit/service/SavingService.java
@@ -17,4 +17,5 @@ public interface SavingService {
 
     void deleteSaving(Long id);
 
+
 }
--- src/main/java/com/example/trackit/web/BudgetController.java
+++ src/main/java/com/example/trackit/web/BudgetController.java
@@ -4,6 +4,7 @@ import com.example.trackit.model.dto.AddBudgetDto;
 import com.example.trackit.model.dto.ViewAllBudgetDto;
 import com.example.trackit.repository.CategoryRepository;
 import com.example.trackit.service.BudgetService;
+import com.example.trackit.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,23 +12,25 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
+import java.net.URLEncoder;
 import java.util.List;
 
 @Controller
 @RequestMapping("/budgets")
 public class BudgetController {
-    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
     private final BudgetService budgetService;
 
-    public BudgetController(CategoryRepository categoryRepository, BudgetService budgetService) {
-        this.categoryRepository = categoryRepository;
+    public BudgetController(CategoryService categoryService, BudgetService budgetService) {
+        this.categoryService = categoryService;
         this.budgetService = budgetService;
     }
 
     @GetMapping("/create")
     public String viewAddBudget(Model model){
-        model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("categories", categoryService.getAllForUser());
         if (!model.containsAttribute("budgetData")){
             model.addAttribute("budgetData", new AddBudgetDto());
         }
@@ -62,7 +65,13 @@ public class BudgetController {
         boolean edited = budgetService.editBudget(category, amount);
         if (!edited){
             redirectAttributes.addFlashAttribute("invalid", true);
-            return "redirect:/budgets/edit/" + category;
+            String encodedCategory = "";
+            try {
+                encodedCategory = URLEncoder.encode(category, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return "redirect:/budgets/edit/" + encodedCategory;
         }
 
         return "redirect:/budgets/all";
