b693e7d918570c5ca709550a2a4a86d06c2be102
Mihaela <mihaelabataklieva2006@gmail.com>
2024-08-03 11:22:31 +0300

    Adding aspect for user registration

--- src/main/java/com/example/trackit/service/TrackRegistrationAspect.java
+++ src/main/java/com/example/trackit/service/TrackRegistrationAspect.java
@@ -1,5 +1,7 @@
 package com.example.trackit.service;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,4 +12,14 @@ import org.springframework.stereotype.Component;
 public class TrackRegistrationAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(TrackRegistrationAspect.class);
 
+    @Around("execution(* com.example.trackit.service.impl.UserServiceImpl.registerUser(..))")
+    public Object trackUserRegistrationLessThan3Sec(ProceedingJoinPoint pjp) throws Throwable {
+       long start = System.currentTimeMillis();
+       Object proceed = pjp.proceed();
+       long executionTime = System.currentTimeMillis() - start;
+       if (executionTime > 3000){
+           LOGGER.warn("Registration took {} ms to execute", executionTime);
+       }
+       return proceed;
+    }
 }
